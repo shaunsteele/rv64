b38728ca4a691fbce5bafbb6ebccc32853ba0458
shaunsteele <shaun.steele.1020@gmail.com>
2024-09-27 00:21:24 -0700

    added modports and fixed typo

--- sim/axi_lite_uvm/axi_lite_if.sv
+++ sim/axi_lite_uvm/axi_lite_if.sv
@@ -21,7 +21,7 @@ logic [2:0]       awprot;
 logic             wvalid;
 logic             wready;
 logic [DLEN-1:0]  wdata;
-logic [SLEN-1:0]  wresp;
+logic [SLEN-1:0]  wstrb;
 
 // write response channel
 logic             bvalid;
@@ -40,5 +40,22 @@ logic             rready;
 logic [DLEN-1:0]  rdata;
 logic [1:0]       rresp;
 
+modport M (
+  input aclk, aresetn,
+  output awvalid, awaddr, awprot, input awready,
+  output wvalid, wdata, wstrb, input wready,
+  output bready, input bvalid, bresp,
+  output arvalid, araddr, arprot, input arready,
+  output rready, input rvalid, rdata, rresp
+);
+
+modport S (
+  input aclk, aresetn,
+  input awvalid, awaddr, awprot, output awready,
+  input wvalid, wdata, wstrb, output wready,
+  input bready, output bvalid, bresp,
+  input arvalid, araddr, arprot, output arready,
+  input rready, output rvalid, rdata, rresp
+);
 
 endinterface
